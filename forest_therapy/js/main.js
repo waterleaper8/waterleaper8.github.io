d812b546a2e5cf5deb557b2d0772bd60042c2d5b
waterleaper <waterleaper8@gmail.com>
2021-09-28 18:03:57 +0900

    Add typing animation

--- forest_therapy/js/main.js
+++ forest_therapy/js/main.js
@@ -1,7 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
   const spinner = document.getElementById('loading');
   const video = document.getElementById('video');
+  const type_ele = document.querySelector('typewritten-text');
   video.addEventListener('canplay', function() {
       spinner.classList.add('loaded');
-  }, false);
-}, false);
+      type_ele.start();
+  }, {
+    once: true
+  });
+});
