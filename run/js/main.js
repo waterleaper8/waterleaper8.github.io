fa5f4dac7c395f756b8daab29cec3ff03dc95471
waterleaper <waterleaper8@gmail.com>
2021-10-10 12:27:07 +0900

    Add redirect new site

--- run/js/main.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-function resize() {
-  let vw = document.body.clientWidth;// スクロールバーを除いた幅を取得
-  document.documentElement.style.setProperty('--vw', vw + 'px');
-}
-
-resize();
-window.onresize = resize;
