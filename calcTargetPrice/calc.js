a8b3e4559acc39d54c8a0b32a8bb671d2fdcdfd0
waterleaper8 <waterleaper8@gmail.com>
2022-07-28 11:56:15 +0900

    設定値保存追加

--- calcTargetPrice/calc.js
+++ calcTargetPrice/calc.js
@@ -3,11 +3,33 @@ const resultLower = document.getElementById("result__lower")
 const upperLine = document.getElementById("upperLine")
 const lowerLine = document.getElementById("lowerLine")
 
-console.log(resultUpper)
-const calcTargetPrice = (e) => {
+const init = () => {
+  upperLine.querySelector(
+    `option[value="${localStorage["upperLine"]}"]`
+  ).selected = true
+  lowerLine.querySelector(
+    `option[value="${localStorage["lowerLine"]}"]`
+  ).selected = true
+}
+
+const saveValue = () => {
+  localStorage.setItem("upperLine", upperLine.value)
+  localStorage.setItem("lowerLine", lowerLine.value)
+}
+
+const calcTargetPrice = () => {
   const purchasePrice = document.getElementById("purchasePrice").value
   const resultUpperNum = purchasePrice * upperLine.value
   const resultLowerNum = purchasePrice * (1 + Number(lowerLine.value))
   resultUpper.innerText = Math.round(resultUpperNum)
   resultLower.innerText = Math.round(resultLowerNum)
+}
+
+const onChangeHandler = () => {
+  saveValue()
+  calcTargetPrice()
+}
+
+window.onload = () => {
+  init()
 }
